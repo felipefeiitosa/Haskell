ed766b7fda1fdd8e8a2d17b9626024881007f6a3
felipefeiitosa <felipe.feiitosa08@gmail.com>
2026-04-16 10:55:23 -0300

    Exercicio 03 - Potencia 2 Concluído

---
+++ exercicio03.hs
@@ -0,0 +1,4 @@
+-- 03 - Escreva uma função potencia_2 que retorne o quadrado de um número (x2).
+
+potencia_2 :: Int -> Int
+potencia_2 n = n * n
---
+++ exercicio04.hs
@@ -0,0 +1,5 @@
+potencia_2 :: Int -> Int
+potencia_2 n = n * n
+
+potencia_4 :: Int -> Int
+potencia_4 n = potencia_2(potencia_2 n)
